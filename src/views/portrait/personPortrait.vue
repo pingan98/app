849be74b1339c4d46be33b898b27d5b119726cf2
shuqiao <shuqiao@unicheers.com>
2023-12-22 15:54:27 +0800

    fix: 个人画像再次重画

--- src/views/portrait/personPortrait.vue
+++ src/views/portrait/personPortrait.vue
@@ -53,31 +53,80 @@ function refreshData(param: any) {
     <van-empty v-if="!bean.police?.jyname" description="暂无数据" />
     <div class="person-portrait-main" v-else>
       <div class="all-count-box">
-        <div class="all-count-item left-box van-hairline--right">
+        <div class="all-count-item left-box">
           <van-image :src="bean.police?.photoPath || avatar" width="50px" />
           <div>
             <div class="name">{{ bean.police?.jyname || "" }}</div>
             <div>{{ bean.orgName || "" }}</div>
           </div>
         </div>
-        <div class="all-count-item right-box">
-          <div class="name">记分总分</div>
-          <div>
-            <span class="num">{{ scoreBean.totalScoreNum || 0 }}</span
-            >分
+        <div class="right-box">
+          <div
+            class="count-item"
+            v-if="Number(bean.punishData.rulePunish || '')"
+          >
+            <span>局纪局规处理</span
+            ><span class="num">{{ bean.punishData.rulePunish || 0 }}</span>
+          </div>
+          <div
+            class="count-item"
+            v-if="Number(bean.punishData.govPunish || '')"
+          >
+            <span>党纪政务处分</span
+            ><span class="num">{{ bean.punishData.govPunish || 0 }}</span>
           </div>
         </div>
       </div>
 
       <div class="flex-module-box flex justify-between items-center mt-[18px]">
         <module-box
-          title="接处警"
+          title="队伍记分"
           bg="from-[#d0eaff] to-[#f9fbff]"
-          class="w-[48%]"
+          class="flex-1"
         >
           <template v-slot:icon>
             <img src="@/assets/sort_icon3@3x.png" alt="" />
           </template>
+          <div class="count-box">
+            <div class="count-item van-hairline--right">
+              <div class="name">数量</div>
+              <div>
+                <span class="num">{{
+                  Number(scoreBean.scoreNum || "") || "-"
+                }}</span>
+              </div>
+            </div>
+            <div class="count-item van-hairline--right">
+              <div class="name">总记分</div>
+              <div>
+                <span class="num">{{
+                  Number(scoreBean.totalScoreNum || "") || "-"
+                }}</span>
+              </div>
+            </div>
+            <div class="count-item">
+              <div class="name">单位排名</div>
+              <div>
+                <span class="num">{{
+                  Number(scoreBean.scoreRank || "") || "-"
+                }}</span>
+              </div>
+            </div>
+          </div>
+        </module-box>
+        <module-box
+          v-if="
+            bean?.caseData?.handlePoliceNumber &&
+            bean?.caseData?.handlePoliceNumber != 0
+          "
+          title="接处警"
+          bg="from-[#d0f7ff] to-[#f7fdff]"
+          class="w-[40%] ml-[10px]"
+        >
+          <template v-slot:icon>
+            <img src="@/assets/sort_icon4@3x.png" alt="" />
+          </template>
+
           <div class="count-box">
             <div class="count-item van-hairline--right">
               <div class="name">数量</div>
@@ -91,29 +140,6 @@ function refreshData(param: any) {
               <div class="name">单位排名</div>
               <div>
                 <span class="num">{{ bean.caseData.ranking || 0 }}</span>
-              </div>
-            </div>
-          </div>
-        </module-box>
-        <module-box
-          title="处分"
-          bg="from-[#d0f7ff] to-[#f7fdff]"
-          class="w-[48%]"
-        >
-          <template v-slot:icon>
-            <img src="@/assets/sort_icon4@3x.png" alt="" />
-          </template>
-          <div class="count-box">
-            <div class="count-item van-hairline--right">
-              <div class="name">局纪局规</div>
-              <div>
-                <span class="num">{{ bean.caseData.rulePunish || 0 }}</span>
-              </div>
-            </div>
-            <div class="count-item">
-              <div class="name">党纪政务</div>
-              <div>
-                <span class="num">{{ bean.caseData.govPunish || 0 }}</span>
               </div>
             </div>
           </div>
@@ -144,30 +170,6 @@ function refreshData(param: any) {
           </div>
         </div>
       </div>
-
-      <!-- <module-box
-        title="执法质态"
-        bg="from-[#d0dfff] to-[#fbfdfd]"
-        class="mt-[18px]"
-      >
-        <template v-slot:icon>
-          <img src="@/assets/sort_icon2@3x.png" alt="" />
-        </template>
-        <div class="count-box">
-          <div class="count-item van-hairline--right">
-            <div class="name">办理刑事案件数</div>
-            <div><span class="num">0</span>起</div>
-          </div>
-          <div class="count-item van-hairline--right">
-            <div class="name">办理行政案件数</div>
-            <div><span class="num">0</span>起</div>
-          </div>
-          <div class="count-item">
-            <div class="name">接处警数</div>
-            <div><span class="num">0</span>起</div>
-          </div>
-        </div>
-      </module-box> -->
 
       <!-- TODO: 此处没找到相关接口 -->
       <module-box title="饮酒报备次数：2" class="mt-[18px]" v-if="false">
@@ -201,10 +203,12 @@ function refreshData(param: any) {
 <style scoped lang="less">
 @import "@/styles/mixin.less";
 @import "./style/index.less";
+
 .person-portrait-page {
   padding-top: 88px;
   // padding-top: 140px;
 }
+
 .page-search {
   position: fixed;
   width: 100%;
@@ -213,10 +217,12 @@ function refreshData(param: any) {
   border-top: 4px solid #e5e7eb;
   background: #fff;
 }
+
 .person-portrait-main {
   background: #ffffff;
   padding: 16px;
 }
+
 .all-count-box {
   .flex(space-between, center);
   padding: 16px;
@@ -225,32 +231,52 @@ function refreshData(param: any) {
   background: linear-gradient(90deg, #0084ff 0%, #02c2fa 100%);
   box-shadow: 0px 2px 6px rgba(60, 130, 254, 0.16);
   border-radius: 5px;
+
   .left-box {
     flex: 1;
     .flex(flex-start, center);
+
     .van-image {
       margin-right: 16px;
     }
+
     .name {
       font-size: 20px;
       font-family: Source Han Sans SC;
       font-weight: bold;
     }
   }
+
   .right-box {
-    width: 40%;
+    flex-shrink: 0;
     text-align: center;
-    .num {
-      font-size: 28px;
-      font-family: DIN;
+
+    .count-item {
+      background: rgba(255, 255, 255, 0.39);
+      height: 26px;
+      width: 130px;
+      border-radius: 3px;
+      color: #ffffff;
+
+      .num {
+        font-size: 18px;
+        font-family: DIN;
+        font-weight: bold;
+        margin-left: 10px;
+      }
+      & + .count-item {
+        margin-top: 10px;
+      }
     }
   }
 }
+
 .count-box {
   .flex(space-evenly, flex-start);
   padding: 10px 0;
   background: #ffffff;
   border-radius: 5px;
+
   .count-item {
     flex: 1;
     .flex(center, center);
@@ -266,23 +292,28 @@ function refreshData(param: any) {
     }
   }
 }
+
 .drink-module {
   width: 100%;
   padding: 10px;
+
   &_table {
     max-height: 280px;
     margin: 0 auto;
     overflow-y: auto;
   }
+
   &_row {
     color: #666666;
     .flex(flex-start, center);
+
     &.is-header {
       height: 36px;
       background: #edeff3;
       font-size: 12px;
     }
   }
+
   &_col {
     text-align: center;
     width: 33.33%;
